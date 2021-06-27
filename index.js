ed61ae2df803fab456d505d0bab4b120abbe4be4
Alex M <milesAwayAlex@gmail.com>
2021-06-26 23:42:08 -0700

    Implement nameSwapper

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 export default nameStr => {
-  if (nameStr === undefined) {
-    throw new Error('Expected string, got undefined');
-  }
+  if (nameStr === undefined) throw new Error('Pass me a string');
+  if (!nameStr) return '';
+  const honors = ['Mr.', 'Mrs.', 'Ms.', 'Dr.'].map(e => e.toUpperCase());
+  const arr = nameStr.replace(/\s+/g, ' ').trim().split(' ');
+  const honored = honors.includes(arr[0].toUpperCase());
+  if (arr.length === 1) return honored ? '' : arr[0];
+  if (arr.length === 2)
+    return honored ? arr.join(' ') : arr.reverse().join(', ');
+  return `${arr[0]} ${arr[2]}, ${arr[1]}`;
 };
